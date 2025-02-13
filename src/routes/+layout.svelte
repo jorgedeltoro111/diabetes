453e32abe3c0b271aa4db5071a8d1bc0fb936fbc
jorgedeltoro111 <ingjorgedeltoro@gmail.com>
2025-02-13 15:23:03 -0600

    Navbar created with routes: about, login and profile

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,2 +1,29 @@
-<h1>navbar</h1>
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <div class="container-fluid">
+        <!-- svelte-ignore a11y_invalid_attribute -->
+        <a class="navbar-brand" href="/">Diabetes360</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+            <li class="nav-item">
+            <!-- svelte-ignore a11y_invalid_attribute -->
+                <a class="nav-link active" aria-current="page" href="/">Inicio</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="profile">Perfil</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="login">Iniciar sesión</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="/">Salir</a>
+            </li>
+        </ul>
+        </div>
+    </div>
+</nav>
+
+
 <slot/>
